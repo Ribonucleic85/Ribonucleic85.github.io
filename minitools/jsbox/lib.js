65a7022bf41e5dba8711feaa97e8379b66ea6341
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2018-11-12 00:07:39 +0000

    Update lib.js

--- minitools/jsbox/lib.js
+++ minitools/jsbox/lib.js
@@ -1,7 +1,22 @@
 const jsbox = (src) => {
-	try { src = document.getElementById(src); }
-	catch(e) { return; }
-	try { eval(src.value); }
-	catch(E) { src.style.backgroundColor = "#fa8072"; }
-	finally { setTimeout(()=>{ src.style.backgroundColor=""; }, 3000); }
+	try {
+		src = document.getElementById(src);
+	}
+	catch(e) {
+		return;
+	}
+
+	var timeout = false;
+
+	try {
+		eval(src.value);
+	}
+	catch(E) {
+		timeout = true;
+		src.style.backgroundColor = "#fa8072";
+	}
+	if (timeout)
+		setTimeout(()=>{ src.style.backgroundColor=""; }, 3000);
+
+	src.focus();
 }
