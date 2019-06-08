d965b6bed0504c2fd5c204678a5abc8bd44d593f
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2019-06-08 01:30:48 +0100

    Update core.js

--- cube/core.js
+++ cube/core.js
@@ -7,13 +7,14 @@ Array.prototype.leftRot = function() {
 	this.push(this.shift());
 	return this;
 }*/
+/*
 Object.prototype.inv = function() {
 	for (let i in this) {
 		if (this.hasOwnProperty(i))
 			i[2] += "'",
 			i[3] += "'";
 	}
-}
+}*/
 /*
 Object.prototype.Lock = function() {
 	Object.freeze(this);
@@ -44,13 +45,10 @@ const trans = (alg, ax) => {
 		"z": [..."URDL"],  "z'": [..."LDRU"]
 	};
 	Midd = {
-		"x": [..."ESES"],  "x'": [..."SESE"],
-		"y": [..."SMSM"],  "y'": [..."MSMS"],
-		"z": [..."EMEM"],  "z'": [..."MEME"]
+		"x": ["E","S","E'","S'"],  "x'": ["S","E","S'","E'"],
+		"y": ["S","M","S'","M'"],  "y'": ["M","S","M'","S'"],
+		"z": ["E","M","E'","M'"],  "z'": ["M","E","M'","E'"]
 	};
-	Midd.inv();
-	// Edge.Lock();
-	// Midd.Lock(); // freezing these might be getting in the way?
 
 	alg = alg.split(" ");
 	cm = 0;
@@ -65,37 +63,22 @@ const trans = (alg, ax) => {
 	mish.rightRot();
 
 	for (; cm<alg.length; cm++) {
-		// ccw move?
 		if (alg[cm].indexOf("'") != -1)
-			ccw = !0, // move is ccw
-			alg[cm] = alg[cm][0]; // convert move to cw
+			ccw = !0,
+			alg[cm] = alg[cm][0];
 
-		// edge move?
-		if (ed.indexOf(alg[cm]) != -1) {
-			alg[cm] = ed[edsh.indexOf(alg[cm])]; // is edge move so transform it to next move in the sequence
-			alg[cm] += ccw? "'": ""; // if move was ccw change it back
-			ccw = !1; // ccw indicator to false, no point in checking what it is, just do it
+		if (ed.indexOf(alg[cm]) != -1)
+			alg[cm] = ed[edsh.indexOf(alg[cm])],
+			alg[cm] += ccw? "'": "",
+			ccw = !1,
 			continue;
-		}
 
-		// middle move?
-		if (mi.indexOf(alg[cm]) != -1) {
-			logBox("middle move routine log, is a move getting through despite the check?");
-			logBox(mi +" & "+ mish +" ; << mi & mish >> check arrays")
-			logBox(alg[cm] +" ; << alg[cm] >> before ccw check");
-			alg[cm] += ccw? "'": "";
-			logBox(alg[cm] +" ; << alg[cm] >> after ccw check, before move shift");
-			ccw = !1;
-			logBox(mish.indexOf(alg[cm]) +" ; << mish.indexOf(alg[cm]) >> before move shift");
-			logBox(alg[cm] +" in "+ mish +"? ; << alg[cm] in mish ? >> before move shift");
-			alg[cm] = mi[mish.indexOf(alg[cm])]; // is middle move so transform it to next move in the sequence
-			logBox(alg[cm] +" ; << alg[cm] >> after move shift");
-			logBox("-----------------------------------------------------------------");
+		alg[cm] += ccw? "'": "";
+		ccw = !1;
+
+		if (mi.indexOf(alg[cm]) != -1)
+			alg[cm] = mi[mish.indexOf(alg[cm])],
 			continue;
-		}
-
-		// if no match is found then the move doesn't need to change
-		// for example in a y transform L, R and M moves are not changed
 	}
 
 	return alg.join(" ");
