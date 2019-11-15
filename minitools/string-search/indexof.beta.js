4469b43ec5b88f3556bc7018e3fb99a06bd2957f
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2019-11-15 15:42:37 +0000

    Update indexof.beta.js
    reformatting for easier legibility and some code re-organising

--- minitools/string-search/indexof.beta.js
+++ minitools/string-search/indexof.beta.js
@@ -19,30 +19,32 @@
 /*************************/
 /*	indexesOf() ;; Sting.prototype
 **
-**	To find line breaks pass sym="\n"
+**	To find line breaks pass searchTerm = "\n"
 **
 **	This searcher keeps adding indexes for the given
-**	symbol and when -1 is added it is popped off by
+**	search term and when -1 is added it is popped off by
 **	the loop conditions and the rest of the indexes
 **	are returned unless there are none in which case
 **	-1 is returned
 */
+const readMe = "typeCheck\n  returns: String, RegExp, Array, Number, Float or False ..\n\ngetLines (lnNo, lnBr)\n  lnNo: Line Number OR Array (for multiple lines)\n  lnBr: Line break Array (optional; can create its own)\n\n  returns: Line content as Strings\n    if just one line it will be a String\n    if multiple lines it will be an Array of Strings\n\nindexOf\n  returns: the first index of the search term passed\n    if not found it returns -1\n\nindexesOf\n  returns: same as indexOf but returns all indexes as\n    an array\n\nperLineIndexesOf\n  returns: same as indexesOf but indexes are returned\n    as a series of sub arrays with a line number in the\n    following format:\n      ret = [ [line, indexes], [line, indexes], .. ]";
 
 
-var typeCheck = (o) => {
+var typeCheck = o => {
 	var ob = o;
 	o = Object.prototype.toString.call(o);
 	return (
-	  o == "[object String]"? "String":
-	  o == "[object RegExp]"? "RegExp":
-	  o == "[object Array]" ? "Array":
-	  o == "[object Number]"
-	  && (isFinite(ob)
-	  && !isNaN(ob))? (
-	    Number.isInteger(ob)? "Number":
-	    /* float */		  "Float"
-	  ):
-	  false
+		o == "[object String]"?
+			"String":
+		o == "[object RegExp]"?
+			"RegExp":
+		o == "[object Array]"?
+			"Array":
+		o == "[object Number]" && (isFinite(ob) && !isNaN(ob))? Number.isInteger(ob)?
+			"Number":
+			"Float"
+		/**/ ):
+			false
 	);
 }
 
@@ -52,29 +54,28 @@ String.prototype.getLines = function(lnNo,lnBr) {
 	if (typeCheck(lnBr)!="Array")
 		lnBr = src.indexesOf("\n");
 	const indexer = lnNm => (
-		(lnNm<=0
-		|| lnBr.length==0)?	[0, src.length]:
-		lnNm==1?		[0, lnBr[0]]:
-		lnNm>=lnBr.length+1?	[lnBr[lnBr.length-1]+1, src.length]:
-		/* else */		[lnBr[lnNm-2]+1, lnBr[lnNm-1]]
+		(lnNm<=0 || lnBr.length==0) ?
+			[0, src.length]:
+		lnNm==1 ?
+			[0, lnBr[0]]:
+		lnNm>=lnBr.length+1 ?
+			[lnBr[lnBr.length-1]+1, src.length]:
+		/* else */ // ?
+			[lnBr[lnNm-2]+1, lnBr[lnNm-1]]
 	);
 	if (typeCheck(lnNo)=="Number")
 		return src.substring(...indexer(lnNo));
 	var l = [];
 	while (lnNo.length)
-		l.push(
-		  src.substring(
-		    ...indexer(lnNo.shift())
-		  )
-		);
+		l.push(src.substring(...indexer(lnNo.shift())));
 	return l;
 }
 
 
 String.prototype.indexof = String.prototype.indexOf;
 String.prototype.indexOf = function(searchTerm,startIndex) {
-	var tc = typeCheck(searchTerm);
 	startIndex = startIndex || 0;
+	var tc = typeCheck(searchTerm);
 	if (tc=="String" || tc=="Number" || tc=="Float")
 		return this.indexof(searchTerm,startIndex);
 	if (tc=="RegExp") {
@@ -94,6 +95,7 @@ String.prototype.indexesOf = function(searchTerm) {
 	return i;
 }
 
+
 String.prototype.perLineIndexesOf = function(searchTerm) {
 	var i = [],
 	lnBr = this.indexesOf("\n");
@@ -110,28 +112,3 @@ String.prototype.perLineIndexesOf = function(searchTerm) {
 	i = [...i[0]];
 	return i;
 }
-
-/*
-String.prototype.perLineIndexesOf = function(searchTerm) {
-	var i = [],
-	lnBr = this.indexesOf("\n");
-	lnBr.unshift(-1);
-	lnBr.push(this.length-1);
-	while (i.length<lnBr.length-1)
-		i.push([
-		  i.length+1,
-		  this.substring(
-		    lnBr[i.length]+1, lnBr[i.length+1]
-		  ).indexesOf(searchTerm)
-		]);
-	i.push([]);
-	i.push([]);
-	while (i.length>2)
-		i[i.length-(i[0][1]==-1? 2: 1)].push(i.shift());
-	i.shift();
-	i = [...i[0]];
-	lnBr.shift();
-	lnBr.pop();
-	return [i, lnBr];
-}
-*/
