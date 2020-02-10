1b6dec27f1edafe356fc1aa3a9e8d87fabb9c91a
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2020-02-10 15:23:24 +0000

    Update lib.js

--- minitools/jsbox/lib.js
+++ minitools/jsbox/lib.js
@@ -20,3 +20,38 @@ const jsbox = (src) => {
 
 	src.focus();
 }
+
+// only tested in chrome, still WIP
+var jsboxAdv = (src) => {
+	try {
+		src = document.getElementById(src);
+		var err = document.getElementById("jsErr");
+		var errBox = document.getElementById("jsErrbox");
+		err.innerText = "";
+		errBox.setAttribute("style","display:none;");
+	}
+	catch(e) {
+		return;
+	}
+
+	var timeout = false;
+
+	try {
+		eval(src.value);
+	}
+	catch(E) {
+		timeout = true;
+		src.style.backgroundColor = "#fa8072";
+
+		// show error text box
+		// error will display till next healthy eval
+		errBox.removeAttribute("style");
+
+		// write error to error text box / old errors are overwritten
+		err.innerText = E.stack;
+	}
+	if (timeout) // vv remove red background after 3 seconds vv
+		setTimeout(()=>{ src.style.backgroundColor=""; }, 3000); 
+
+	src.focus();
+}
