bf3102a41f68b1132802e7ac1c657ba70556b024
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2020-02-19 23:53:47 +0000

    Update trans.new.js

--- cube/trans.new.js
+++ cube/trans.new.js
@@ -1,55 +1,30 @@
-// need to reduce variable list
-
 var trans = (alg, ax) => {
-	var Edge, Midd, cm, cur, ca, ii, ed, edsh, mi, mish, getmove,
-		EdgeLc, MiddLc, edLc, edshLc, miLc, mishLc;
-
+	var Edge, Midd;
 	Edge = {
-		"x": [..."FUBD"],  "x'": [..."DBUF"],
-		"y": [..."FLBR"],  "y'": [..."RBLF"],
-		"z": [..."URDL"],  "z'": [..."LDRU"]
-	};
-	EdgeLc = {
-		"x": [..."fubd"],  "x'": [..."dbuf"],
-		"y": [..."flbr"],  "y'": [..."rblf"],
-		"z": [..."urdl"],  "z'": [..."ldru"]
+		"x": [[..."FUBD"],[..."fubd"]], "x'": [[..."DBUF"],[..."dbuf"]],
+		"y": [[..."FLBR"],[..."flbr"]], "y'": [[..."RBLF"],[..."rblf"]],
+		"z": [[..."URDL"],[..."urdl"]], "z'": [[..."LDRU"],[..."ldru"]]
 	};
 	Midd = {
-		"x": ["E","S","E'","S'"],  "x'": ["S","E","S'","E'"],
-		"y": ["S","M","S'","M'"],  "y'": ["M","S","M'","S'"],
-		"z": ["E","M","E'","M'"],  "z'": ["M","E","M'","E'"]
+		"x": [["E","S","E'","S'"],["e","s","e'","s'"]], "x'": [["S","E","S'","E'"],["s","e","s'","e'"]],
+		"y": [["S","M","S'","M'"],["s","m","s'","m'"]], "y'": [["M","S","M'","S'"],["m","s","m'","s'"]],
+		"z": [["E","M","E'","M'"],["e","m","e'","m'"]], "z'": [["M","E","M'","E'"],["m","e","m'","e'"]]
 	};
-	MiddLc = {
-		"x": ["e","s","e'","s'"],  "x'": ["s","e","s'","e'"],
-		"y": ["s","m","s'","m'"],  "y'": ["m","s","m'","s'"],
-		"z": ["e","m","e'","m'"],  "z'": ["m","e","m'","e'"]
-	};
-
-	ed   = [...Edge[ax]]; edsh = [...Edge[ax]]; edsh.rightRot();
-	edLc   = [...EdgeLc[ax]]; edshLc = [...EdgeLc[ax]]; edshLc.rightRot();
-
-	mi   = [...Midd[ax]]; mish = [...Midd[ax]]; mish.rightRot();
-	miLc   = [...MiddLc[ax]]; mishLc = [...MiddLc[ax]]; mishLc.rightRot();
+	var ed, edsh, mi, mish;
+	ed   = [[...Edge[ax][0]],[...Edge[ax][1]]];
+	edsh = [ ...([...ed[0]].rightRot()), ...([...ed[1]].rightRot()) ];
+	ed   = [ ...ed[0], ...ed[1] ];
+	mi   = [[...Midd[ax][0]],[...Midd[ax][1]]];
+	mish = [...([...mi[0]].rightRot()), ...([...mi[1]].rightRot())];
+	mi   = [...mi[0], ...mi[1]];
 
 	alg = alg.split(/\s/);
-	cm = 0;
-
-	for (; cm<alg.length; cm++) {
-		cur = alg[cm].match(/[A-Za-z]/g).pop(); //getmove(alg[cm]);
-		logBox("init "+cm +"\ncur "+cur);
-
-		if (!cur)
-			continue;
-
+	for (var cur,cm=0; cm<alg.length; cm++) {
+		cur = alg[cm].match(/[A-Za-z]/g).pop();
 		if (ed.indexOf(cur) != -1)
 			alg[cm] = alg[cm].replace(cur, ed[edsh.indexOf(cur)]);
 		else if (mi.indexOf(cur) != -1)
 			alg[cm] = alg[cm].replace(cur, mi[mish.indexOf(cur)]);
-
-		else if (edLc.indexOf(cur) != -1)
-			alg[cm] = alg[cm].replace(cur, edLc[edshLc.indexOf(cur)]);
-		else if (miLc.indexOf(cur) != -1)
-			alg[cm] = alg[cm].replace(cur, miLc[mishLc.indexOf(cur)]);
 	}
 
 	return alg.join(" ");
