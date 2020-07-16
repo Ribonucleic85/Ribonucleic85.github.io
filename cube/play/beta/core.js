99554e57be1baba9b7c24b4ea2adbaa337ffb1ca
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2020-07-16 21:19:48 +0100

    Update core.js

--- cube/play/beta/core.js
+++ cube/play/beta/core.js
@@ -10,18 +10,11 @@ const MatrixEq = (x, xl, xi, y, yl, yi, sqrt, m) => {
 	return arr;
 }
 
-/* semi-compact if-less version */
-const Matrix = (matrix,mode,direction=1,rowcol) => {
-	var sqrt = (matrix.length**0.5) >> 0;
-
-	return MatrixEq(...
-	/* -90 */	[[[0,sqrt,1,sqrt-1,-1,-1,sqrt,matrix],
-	/* 90  */	[sqrt-1,-1,-1,0,sqrt,1,sqrt,matrix],
-	/* 180 */	[0,1,1,sqrt**2-1,-1,-1,sqrt,matrix]],[
-	/* -r  */	[rowcol,rowcol+1,1,sqrt-1,-1,-1,sqrt,matrix],
-	/* r   */	[rowcol,rowcol+1,1,0,sqrt,1,sqrt,matrix]],[
-	/* -c  */	[sqrt-1,-1,-1,rowcol,rowcol+1,1,sqrt,matrix],
-	/* c   */	[0,sqrt,1,rowcol,rowcol+1,1,sqrt,matrix]]
-		][["rot","row","col"].indexOf(mode)][[-1,1,2].indexOf(direction)]
-	);
+/* compact if-less version */
+const Matrix = (x,m,d=1,r) => {
+	const s = x.length**0.5>>0;
+	/* -90, 90, 180, -row, row, -col, col */
+	return MatrixEq(...[[[0,s,1,s-1,-1,-1,s,x],[s-1,-1,-1,0,s,1,s,x],[0,1,1,s**2-1,-1,-1,s,x]],
+	[[r,r+1,1,s-1,-1,-1,s,x],[r,r+1,1,0,s,1,s,x]],[[s-1,-1,-1,r,r+1,1,s,x],[0,s,1,r,r+1,1,s,x]]][
+	["rot","row","col"].indexOf(m)][[-1,1,2].indexOf(d)]);
 }
