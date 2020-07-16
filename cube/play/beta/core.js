24e8dcb84ae3992e24703b2a30fc967e30cf0e3f
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2020-07-16 20:24:46 +0100

    another for commit history

--- cube/play/beta/core.js
+++ cube/play/beta/core.js
@@ -10,62 +10,18 @@ const MatrixEq = (x, xl, xi, y, yl, yi, sqrt, m) => {
 	return arr;
 }
 
-/* if-less version */
+/* semi-compact if-less version */
 const Matrix = (matrix,mode,direction=1,rowcol) => {
 	var sqrt = (matrix.length**0.5) >> 0;
 
 	return MatrixEq(...
-	[
-		[
-			[0,       sqrt,      1,   sqrt-1,     -1,        -1,  sqrt,  matrix],
-			[sqrt-1,  -1,        -1,  0,          sqrt,      1,   sqrt,  matrix],
-			[0,       1,         1,   sqrt**2-1,  -1,        -1,  sqrt,  matrix]
-		],[
-			[rowcol,  rowcol+1,  1,   sqrt-1,     -1,        -1,  sqrt,  matrix],
-			[rowcol,  rowcol+1,  1,   0,          sqrt,      1,   sqrt,  matrix]
-		],[
-			[sqrt-1,  -1,        -1,  rowcol,     rowcol+1,  1,   sqrt,  matrix],
-			[0,       sqrt,      1,   rowcol,     rowcol+1,  1,   sqrt,  matrix]
-		]
-	][
-		["rot","row","col"].indexOf(mode)
-	][
-		[-1,1,2].indexOf(direction)
-	] );
+	/* -90 */	[[[0,sqrt,1,sqrt-1,-1,-1,sqrt,matrix],
+	/* 90  */	[sqrt-1,-1,-1,0,sqrt,1,sqrt,matrix],
+	/* 180 */	[0,1,1,sqrt**2-1,-1,-1,sqrt,matrix]],[
+	/* -r  */	[rowcol,rowcol+1,1,sqrt-1,-1,-1,sqrt,matrix],
+	/* r   */	[rowcol,rowcol+1,1,0,sqrt,1,sqrt,matrix]],[
+	/* -c  */	[sqrt-1,-1,-1,rowcol,rowcol+1,1,sqrt,matrix],
+	/* c   */	[0,sqrt,1,rowcol,rowcol+1,1,sqrt,matrix]]
+		][["rot","row","col"].indexOf(mode)][[-1,1,2].indexOf(direction)]
+	);
 }
-
-
-
-
-/* old if based version
-const Matrix = (matrix,mode,direction=1,rowcol) => {
-	var sqrt = (matrix.length**0.5) >> 0;
-	if (mode=="rot") {
-		if (direction==-1) {
-			return MatrixEq(0,sqrt,1, sqrt-1,-1,-1, sqrt, matrix);
-		}
-		else if (direction==1) {
-			return MatrixEq(sqrt-1,-1,-1, 0,sqrt,1, sqrt, matrix);
-		}
-		else if (direction==2) {
-			return MatrixEq(0,1,1, sqrt**2-1,-1,-1, sqrt, matrix);
-		}
-	}
-	else if (mode=="row") {
-		if (direction==1) {
-			return MatrixEq(rowcol,rowcol+1,1, 0,sqrt,1, sqrt, matrix);
-		}
-		else if (direction==-1) {
-			return MatrixEq(rowcol,rowcol+1,1, sqrt-1,-1,-1, sqrt, matrix);
-		}
-	}
-	else if (mode=="col") {
-		if (direction==1) {
-			return MatrixEq(0,sqrt,1, rowcol,rowcol+1,1, sqrt, matrix);
-		}
-		else if (direction==-1) {
-			return MatrixEq(sqrt-1,-1,-1, rowcol,rowcol+1,1, sqrt, matrix);
-		}
-	}
-}
-*/
