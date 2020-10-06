7040e4424c128138ce52eb6eabdcce037a0f0ae9
Ribonucleic85 <33126938+Ribonucleic85@users.noreply.github.com>
2020-10-06 15:38:07 +0100

    Update core.js

--- cube/play/beta/core.js
+++ cube/play/beta/core.js
@@ -1,20 +1,27 @@
-const MatrixEq = (x, xl, xi, y, yl, yi, sqrt, m) => {
+function* Matrix(m,o,d=1,r) {
+	const s = m.length**0.5>>0;
+
+	r==undefined || r--;
+
+	/* -90, 90, 180, -row, row, -col, col */
+	var x,xl,xi,y,yl,yi; 
+	[x,xl,xi,y,yl,yi] = [
+		[[s-1,-1,-1,0,s,1], [0,s,1,s-1,-1,-1], [m.length-1,-1,-1,0,1,1,]],
+		[[s-1,-1,-1,r,r+1,1], [0,s,1,r,r+1,1]],
+		[[r,r+1,1,s-1,-1,-1], [r,r+1,1,0,s,1]]
+	][
+		["rot","row","col"].indexOf(o)
+	][
+		[-1,1,2].indexOf(d)
+	];
+
 	var xr = x
-	,   yr = y
-	,   arr = [];
-	for (y=yr; y!=yl; y+=yi) {
-		for (x=xr; x!=xl; x+=xi) {
-			arr.push(m[x*sqrt+y]);
+	,   yr = y;
+
+	for (x=xr; x!=xl; x+=xi) {
+		for (y=yr; y!=yl; y+=yi) {
+			yield [m[y*s+x], y*s+x];
 		}
 	}
-	return arr;
-}
-
-/* compact if-less version */
-const Matrix = (x,m,d=1,r) => {
-	const s = x.length**0.5>>0;
-	/* -90, 90, 180, -row, row, -col, col */
-	return MatrixEq(...[[[0,s,1,s-1,-1,-1,s,x],[s-1,-1,-1,0,s,1,s,x],[0,1,1,s**2-1,-1,-1,s,x]],
-	[[r,r+1,1,s-1,-1,-1,s,x],[r,r+1,1,0,s,1,s,x]],[[s-1,-1,-1,r,r+1,1,s,x],[0,s,1,r,r+1,1,s,x]]][
-	["rot","row","col"].indexOf(m)][[-1,1,2].indexOf(d)]);
+	return;
 }
